76ec6fbbeb236ae243e108498d201749c6f16dd0
TilekRai <venturesometika@gmail.com>
2021-10-19 00:11:38 +0600

    hm2

--- bin/lesson5.dart
+++ bin/lesson5.dart
@@ -1,6 +1,7 @@
 void main(List<String> arguments) {
   print('Hello world!');
 
+  // Task 1
   var j = 1;
 
   if (j == 1) {
@@ -23,6 +24,7 @@ void main(List<String> arguments) {
     print('мизинец');
   }
 
+  // Task 2
   var sum = 0;
   var z = [2, 3, 4, 5];
   for (int i = 0; i < z.length; i++) {
@@ -30,18 +32,49 @@ void main(List<String> arguments) {
   }
   print(sum);
 
+  // Task 3
   var p = 1;
   if (p >= 0 && p <= 15) {
     print("число $p находится в первой четверти");
   }
-  if (p >= 0 && p <= 30) {
+  if (p >= 15 && p <= 30) {
     print("число $p находится во второй четверти");
   }
-  if (p >= 0 && p <= 45) {
+  if (p >= 31 && p <= 45) {
     print("число $p находится в третьей четверти");
   }
-  if (p >= 0 && p <= 59) {
+  if (p >= 46 && p <= 59) {
     print("число $p находится в четвертой четверти");
-    
   }
-}
+
+  // Task 4
+  var arr = [12, 15, 20, 25, 59, 79];
+  var aw = 0;
+  for (int i = 0; i < arr.length; i++) {
+    aw += arr[i];
+  }
+  var result = aw / arr.length;
+  print(result);
+
+  // Task 5
+  List sa = ["a", "b", "c"];
+  List re = [1, 2, 3];
+  var newList = [...sa, ...re];
+  print(newList);
+
+  // Task 6
+  var lang;
+  final List list1 = [];
+  if (lang == 'ru') {
+    var lang = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+    print(list1);
+  } else if (lang == 'en') {
+    var lang = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+    print(list1);
+  }
+
+  // Additional task
+
+ 
+  }
+
